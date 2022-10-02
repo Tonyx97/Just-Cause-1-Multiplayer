f66b0032accc9a4661edbd32a0bca34be83ce92b
Tony Pérez <tonyx97@hotmail.es>
2022-10-02 13:30:49 +0200

    Update main.cpp

--- client/code/main.cpp
+++ client/code/main.cpp
@@ -265,10 +265,10 @@ DEFINE_HOOK_THISCALL_S(tick, 0x4036F0, bool, void* _this)
 		else if (!game_focused && game_hwnd == GetActiveWindow())
 			g_key->block_input(!(game_focused = true));
 
-#ifdef JC_DBG
+//#ifdef JC_DBG
 		if (g_key->is_key_pressed(VK_F3))
 			g_game_status->load_game();
-#endif
+//#endif
 
 		g_game_control->on_tick();
 	}
