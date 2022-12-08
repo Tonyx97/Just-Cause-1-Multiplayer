156caf4f602733d3f7dc8adc7cdb75c25da131ef
Tony Pérez <tonyx97@hotmail.es>
2022-12-08 17:13:30 +0100

    Update script_events.h

--- scripting/script/lua_ctx/script_events.h
+++ scripting/script/lua_ctx/script_events.h
@@ -3,20 +3,57 @@
 namespace script::event
 {
 #ifdef JC_CLIENT
+	//
 	static inline std::string ON_RENDER = "onRender";
+
+	//
 	static inline std::string ON_PRE_RENDER = "onPreRender";
+
+	// params: Player
+	//
 	static inline std::string ON_PLAYER_JOIN = "onClientPlayerJoin";
-	static inline std::string ON_PLAYER_QUIT= "onClientPlayerQuit";
+
+	// params: Player
+	//
+	static inline std::string ON_PLAYER_QUIT = "onClientPlayerQuit";
+
+	// params: Player
+	//
 	static inline std::string ON_PLAYER_KILLED = "onClientPlayerKilled";
+
+	// params: Resource
+	//
 	static inline std::string ON_RSRC_START = "onClientResourceStart";
+
+	// params: Resource
+	//
 	static inline std::string ON_RSRC_STOP = "onClientResourceStop";
+
+	// params: Player, Pickup
+	//
 	static inline std::string ON_PICKUP_HIT = "onClientPickupHit";
 #else
+	//
 	static inline std::string ON_TICK = "onTick";
+
+	// params: Player
+	//
 	static inline std::string ON_PLAYER_JOIN = "onPlayerJoin";
+
+	// params: Player
+	//
 	static inline std::string ON_PLAYER_QUIT = "onPlayerQuit";
+
+	// params: Player
+	//
 	static inline std::string ON_PLAYER_KILLED = "onPlayerKilled";
+
+	// params: Resource
+	//
 	static inline std::string ON_RSRC_START = "onResourceStart";
+
+	// params: Resource
+	//
 	static inline std::string ON_RSRC_STOP = "onResourceStop";
 #endif
 }
